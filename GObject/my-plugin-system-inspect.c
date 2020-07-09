2771bcd11a0677f611869defafc10f5396f220b3
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2020-07-09 21:50:08 +0200

    + obj inspection

--- GObject/my-plugin-system-inspect.c
+++ GObject/my-plugin-system-inspect.c
@@ -6,7 +6,8 @@
 #include <glib/gstdio.h>
 #include <gmodule.h>
 
-int main (int argc, char ** argv)
+int
+main (int argc, char **argv)
 {
   GModule *module = NULL;
   gpointer ptr;
@@ -35,8 +36,40 @@ int main (int argc, char ** argv)
     GTypeQuery query;
     GType plugin_type = get_type_f ();
 
+    g_printf ("=============  '%s':\n", argv[1]);
     g_type_query (plugin_type, &query);
     g_printf ("GType name = '%s'\n", query.type_name);
+
+    if (G_TYPE_IS_OBJECT (plugin_type)) {
+      GObject *obj;
+      GParamSpec **properties;
+      guint n_properties, p;
+
+      g_printf ("GType is a GObject.\n");
+
+      obj = g_object_new (plugin_type, NULL);
+
+      properties =
+          g_object_class_list_properties (G_OBJECT_GET_CLASS (obj),
+          &n_properties);
+
+      for (p = 0; p < n_properties; p++) {
+        GParamSpec *prop = properties[p];
+        gchar *def_val;
+
+        g_printf ("* Property: '%s', '%s', '%s'\n",
+            g_param_spec_get_blurb (prop),
+            g_param_spec_get_nick (prop), g_param_spec_get_name (prop));
+
+        def_val =
+            g_strdup_value_contents (g_param_spec_get_default_value (prop));
+        g_printf ("\tDefault value: %s\n", def_val);
+        g_free (def_val);
+      }
+
+      g_object_unref (obj);
+      g_free (properties);
+    }
   }
 
   ret = 0;
